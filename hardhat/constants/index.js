451ebfdeefcf7b9188bb1e67cf7ce353761ffd17
CFC Tech <cfctech07@gmail.com>
2023-12-06 20:48:01 +0530

    Initial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x5Aee10035E5d5901F78CA4955c26ce467b28EA4B";
+// URL from where we extract the metadata for the Crypto Devs NFTs
+const METADATA_URL = "https://nft-collection-hellosumitg.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
